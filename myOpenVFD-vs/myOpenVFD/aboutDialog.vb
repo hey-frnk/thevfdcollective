2239cc38b53f1543571969b97a556c0576cb16f8
Frank Fu Zheng <35289370+hey-frnk@users.noreply.github.com>
2018-04-13 06:42:23 +0200

    added myOpenVFD MS visual studio project

---
+++ myOpenVFD-vs/myOpenVFD/aboutDialog.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Windows.Forms
+
+Public Class aboutDialog
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub aboutDialog_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub aboutDialog_Click(sender As Object, e As EventArgs) Handles MyBase.Click
+        System.Diagnostics.Process.Start("https://www.thevfdcollective.com")
+        Me.Close()
+    End Sub
+End Class
